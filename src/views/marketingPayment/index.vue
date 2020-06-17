1924256f55aa10ff274f22b25df14c0c591af0fb
alwaysrunning <178224406@qq.com>
2020-06-17 21:29:30 +0800

    添加收款人全称

--- src/views/marketingPayment/index.vue
+++ src/views/marketingPayment/index.vue
@@ -40,13 +40,24 @@
         type="number"
         :rules="[{ required: true, message: '请输入合同金额' }]"
       />
-      <van-field
-        v-model="payeeName"
-        label="*收款人全称"
-        class="mod-field"
-        placeholder="请输入收款人全称"
-        :rules="[{ required: true, message: '请输入收款人全称' }]"
-      />
+      <div class="mod-select">
+        <div class="mod-label">*收款人全称</div>
+        <el-select
+          v-model="payeeName"
+          filterable
+          remote
+          reserve-keyword
+          placeholder="请输入收款人全称"
+          :remote-method="remoteMethod"
+          :loading="loading">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item">
+          </el-option>
+        </el-select>
+      </div>
       <van-field
         v-model="payeeAccount"
         label="*收款人账号"
@@ -121,6 +132,7 @@
 import Vue from 'vue'
 import { mapGetters } from 'vuex'
 import { Popup, Picker, Button, Form, field, Uploader, Toast } from 'vant'
+import { Select, Option } from 'element-ui'
 Vue.use(Popup)
 Vue.use(Picker)
 Vue.use(Button)
@@ -128,6 +140,8 @@ Vue.use(Uploader)
 Vue.use(Form)
 Vue.use(field)
 Vue.use(Toast)
+Vue.use(Select)
+Vue.use(Option)
 export default {
   data() {
     return {
@@ -148,17 +162,54 @@ export default {
       showPicker: false,
       showPayType: false,
       columns: [],
-      payTypeColumns: []
+      payTypeColumns: [],
+      options: [],
+      list: [],
+      loading: false,
+      states: ["Alabama", "Alaska", "Arizona",
+      "Arkansas", "California", "Colorado",
+      "Connecticut", "Delaware", "Florida",
+      "Georgia", "Hawaii", "Idaho", "Illinois",
+      "Indiana", "Iowa", "Kansas", "Kentucky",
+      "Louisiana", "Maine", "Maryland",
+      "Massachusetts", "Michigan", "Minnesota",
+      "Mississippi", "Missouri", "Montana",
+      "Nebraska", "Nevada", "New Hampshire",
+      "New Jersey", "New Mexico", "New York",
+      "North Carolina", "North Dakota", "Ohio",
+      "Oklahoma", "Oregon", "Pennsylvania",
+      "Rhode Island", "South Carolina",
+      "South Dakota", "Tennessee", "Texas",
+      "Utah", "Vermont", "Virginia",
+      "Washington", "West Virginia", "Wisconsin",
+      "Wyoming"]
     }
   },
   created() {
     this.getMyProjectList()
     this.getPayType()
+    this.list = this.states.map(item => {
+      return { value: `value:${item}`, label: `label:${item}` };
+    })
   },
   computed: {
     ...mapGetters(['userInfo'])
   },
   methods: {
+    remoteMethod(query) {
+      if (query !== '') {
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false;
+          this.options = this.list.filter(item => {
+            return item.label.toLowerCase()
+              .indexOf(query.toLowerCase()) > -1;
+          });
+        }, 200);
+      } else {
+        this.options = [];
+      }
+    },
     getPayType() {
       this.$http.get('/sys/dict/getDictItems/oa_pay_type').then(res => {
         this.payTypeColumns = res.result
