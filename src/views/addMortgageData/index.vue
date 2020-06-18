45aeb36267e849af397218ac1f770872389f7b75
haitao.yang <haitao.yang@haitaoyangdeMacBook-Pro.local>
2020-06-18 18:15:57 +0800

    按揭数据添加

--- src/views/addMortgageData/index.vue
+++ src/views/addMortgageData/index.vue
@@ -1,32 +1,110 @@
 <template>
-  <section class="mod-report">
-    <ul class="mod-application">
-      <li @click="goMorgageReport()">
-        <span>按揭报表</span>
-        <i class="iconfont arrow">&#xe7e3;</i>
-      </li>
-      <li @click="goSaleReport()">
-        <span>销售报表</span>
-        <i class="iconfont arrow">&#xe7e3;</i>
-      </li>
-    </ul>
+  <section class="mod-addMortgageData">
+    <div class="application">
+      <div class="person">申请人</div>
+      <div class="role">{{userInfo.realname}}</div>
+    </div>
+    <van-field
+      readonly
+      clickable
+      class="mod-field"
+      name="picker"
+      :value="departName"
+      label="*所在项目部"
+      :rules="[{ required: true, message: '请选择项目部' }]"
+      right-icon="arrow"
+      placeholder="请选择项目部"
+      @click="showPicker = true"
+    />
+    <div class="mod-module">
+      <div class="subtitle">一期</div>
+      <div class="mod-form">
+        <div class="verify">
+          <label for="username">*全款</label>
+          <input class="verify-input" placeholder="请输入数量" type="text" name="username">数
+          <input class="verify-input" placeholder="请输入金额" type="text" name="username">万元
+        </div>
+        <div class="verify">
+          <label for="username">*待借合同</label>
+          <input class="verify-input" placeholder="请输入数量" type="text" name="username">数
+          <input class="verify-input" placeholder="请输入金额" type="text" name="username">万元
+        </div>
+        <div class="verify">
+          <label for="username">*已放款</label>
+          <input class="verify-input" placeholder="请输入数量" type="text" name="username">数
+          <input class="verify-input" placeholder="请输入金额" type="text" name="username">万元
+        </div>
+        <div class="verify">
+          <label for="username">*待放款</label>
+          <input class="verify-input" placeholder="请输入数量" type="text" name="username">数
+          <input class="verify-input" placeholder="请输入金额" type="text" name="username">万元
+        </div>
+      </div>
+    </div>
+    <van-button type="default" style="margin: 30px 0px 100px 200px">新增期数</van-button>
+    <div style="margin: 16px;">
+      <van-button square block type="info" color="#000" native-type="submit">
+        提交
+      </van-button>
+    </div>
+    <van-popup v-model="showPicker" position="bottom">
+      <van-picker
+        show-toolbar
+        value-key="departName"
+        :columns="columns"
+        @confirm="onConfirm"
+        @cancel="showPicker = false"
+      />
+    </van-popup>
   </section>
 </template>
 
 <script>
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+import { Popup, Picker, Button,  field, Toast } from 'vant'
+Vue.use(Popup)
+Vue.use(Picker)
+Vue.use(Button)
+Vue.use(field)
+Vue.use(Toast)
 export default {
   data() {
     return {
+      departName: '',
+      orgCode: '',
+      showPicker: false,
+      columns: []
     }
   },
   created() {
+    this.getMyProjectList()
+  },
+  computed: {
+    ...mapGetters(['userInfo'])
   },
   methods: {
-    goMorgageReport(path) {
-      this.$router.push('/mortgageStatistic')
+    getMyProjectList() {
+      this.$http.get('/sys/sysDepart/queryMyProjectList').then(res => {
+        this.columns = res.result
+      })
     },
-    goSaleReport() {
-      this.$router.push('/')
+    onConfirm(item) {
+      this.departName = item.departName
+      this.orgCode = item.orgCode
+      this.showPicker = false
+    },
+    onSubmit() {
+      this.$http.post('/djfk/flowDjfkRefund/add', {
+        projectCode: this.orgCode
+      }).then(res => {
+        if (res.success) {
+          Toast.success('保存成功')
+          this.$router.push('/')
+        } else {
+          Toast.fail(res.message)
+        }
+      })
     }
   }
 };
